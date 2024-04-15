b5981b55a85af4b66c6424ba950dd127013ac519
WatarizZz-tests <120670228+WatarizZz-tests@users.noreply.github.com>
2024-04-15 15:30:49 +0100

    Update HeroSlider.jsx

--- src/components/slider/HeroSlider.jsx
+++ src/components/slider/HeroSlider.jsx
@@ -63,26 +63,6 @@ const HeroSlider = () => {
           </div>
         </SwiperSlide>
       </Swiper>
-
-      {/* <div className="hero__slider">
-    
-
-     
-
-    
-      <div className="slider__item slider__item-04 mt0">
-        <div className="Container">
-          <div className="slider__content ">
-            <h4 className="text-light mb-3">For Rent $70 Per Day</h4>
-            <h1 className="text-light mb-4">Reserve Now and Get 50% Off</h1>
-
-            <button className="btn reserve__btn mt-4">
-              <Link to="/cars">Reservaaaaaaaaaaa</Link>
-            </button>
-          </div>
-        </div>
-      </div>
-    </div> */}
     </>
   );
 };
