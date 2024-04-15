dc47fbe9f3b8ac7a66901376bfcde3dc43ba96cb
WatarizZz-tests <120670228+WatarizZz-tests@users.noreply.github.com>
2024-04-15 11:42:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Apropos from '../src/pages/apropos/Apropos';
 import Demande from './pages/demande/Demande';
 import ScrollToTop from './components/scroll/ScrollToTop';
 import InactivityTimeout from '../src/components/InactivityTimeout/InactivityTimeout';
+import ForgotPassword from './pages/forgotpassword/ForgotPassword';
+import NewPassword from './pages/newpassword/NewPassword';
 
 
 function App() {
@@ -22,6 +24,8 @@ function App() {
      <Route path="/formulaireaccident" element={<Accident/>} />
      <Route path="/apropos" element={<Apropos/>} />
      <Route path="/mesdemandes" element={<Demande/>} />
+     <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+     <Route path="/reset_password/:id/:token" element={<NewPassword />}></Route>
      </Routes>
      </InactivityTimeout>
     </BrowserRouter>
