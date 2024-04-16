c41e0c006f52448dbc14281a1b4b53a6d7afeb4d
WatarizZz-tests <120670228+WatarizZz-tests@users.noreply.github.com>
2024-04-16 10:56:12 +0100

    Update ForgotPassword.jsx

--- src/pages/forgotpassword/ForgotPassword.jsx
+++ src/pages/forgotpassword/ForgotPassword.jsx
@@ -14,24 +14,24 @@ function ForgotPassword() {
 
     axios.defaults.withCredentials = true;
     const handleSubmit = (e) => {
-    e.preventDefault();
-    axios.post(`${BASE_URL}/api/auth/forgot-password`, { email })
-        .then(res => {
-            if (res.status === 200) {
-                setConfirmationMessage("Votre email de réinitialisation a été envoyé ! Votre lien sera valide pour une durée de 24 heures.");
-            } else {
-               
-                if (res.status === 500) {
-                    setConfirmationMessage("L'utilisateur n'a pas été trouvé. Veuillez vérifier l'adresse e-mail.");
-                } else {
-                    console.log("Unexpected response status:", res.status);
-                }
-            }
-        })
-        .catch(err => {
-            console.error("Error:", err);
-        });
-};
+      e.preventDefault();
+      axios.post(`${BASE_URL}/api/auth/forgot-password`, { email })
+          .then(res => {
+              if (res.status === 200) {
+                  setConfirmationMessage("Votre email de réinitialisation a été envoyé ! Votre lien sera valide pour une durée de 24 heures.");
+              } else {
+                  // Handle status code 500 (Internal Server Error)
+                  if (res.status === 500) {
+                      setConfirmationMessage("L'utilisateur n'a pas été trouvé. Veuillez vérifier l'adresse e-mail.");
+                  } else {
+                      console.log("Unexpected response status:", res.status);
+                  }
+              }
+          })
+          .catch(err => {
+              console.error("Error:", err);
+          });
+  };
 
     return(
         <>
